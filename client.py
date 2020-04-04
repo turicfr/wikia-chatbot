c7cb7e11fb02246cd8105c1ebed1669608ad299d
turicfr <turicfr@gmail.com>
2020-04-04 21:06:58 +0300

    General changes

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import json
 from urllib.parse import urlencode, urlparse, urlunparse
-from abc import ABC, abstractmethod
+from abc import ABC
+
 import requests
 import socketio
 
@@ -8,34 +9,19 @@ class ClientError(Exception):
     pass
 
 class Client(ABC):
-    def __init__(self, username, password, site, youtube_key=None):
+    def __init__(self, username, password, site):
         self.username = username
         self.password = password
         self.site = site
-        self.youtube_key = youtube_key
         self.session = requests.Session()
         self.sio = socketio.Client()
         for event, handler in {
-            "connect": self.connect,
-            "connect_error": self.connect_error,
-            "disconnect": self.disconnect,
+            "connect": self.on_connect,
+            "connect_error": self.on_connect_error,
+            "disconnect": self.on_disconnect,
             "message": self.on_event,
         }.items():
             self.sio.on(event, handler)
-
-    def _wikia_request(self):
-        return self.session.get(self.site + "wikia.php", params={
-            "controller": "Chat",
-            "format": "json"
-        }).json()
-
-    def _api_request(self):
-        return self.session.get(self.site + "api.php", params={
-            "action": "query",
-            "meta": "siteinfo",
-            "siprop": "wikidesc",
-            "format": "json"
-        }).json()
 
     def login(self):
         print(f"Logging in as {self.username}...")
@@ -43,15 +29,28 @@ class Client(ABC):
             "action": "login",
             "lgname": self.username,
             "lgpassword": self.password,
-            "format": "json"
+            "format": "json",
         }
         response = self.session.post(self.site + "api.php", data=data).json()
         data["lgtoken"] = response["login"]["token"]
         response = self.session.post(self.site + "api.php", data=data).json()
-        if response["login"]["result"] != "Success":
-            raise ClientError("Log in failed")
-        wikia_data = self._wikia_request()
-        api_data = self._api_request()
+        result = response["login"]["result"]
+        if result != "Success":
+            raise ClientError(f"Log in failed: {result}")
+        self.edit_tokens()
+        self.connect()
+
+    def connect(self):
+        wikia_data = self.session.get(self.site + "wikia.php", params={
+            "controller": "Chat",
+            "format": "json",
+        }).json()
+        api_data = self.session.get(self.site + "api.php", params={
+            "action": "query",
+            "meta": "siteinfo",
+            "siprop": "wikidesc",
+            "format": "json",
+        }).json()
         url = list(urlparse(f'https://{wikia_data["chatServerHost"]}/socket.io/'))
         url[4] = urlencode({
             "name": self.username,
@@ -61,6 +60,68 @@ class Client(ABC):
             "wikiId": api_data["query"]["wikidesc"]["id"],
         })
         self.sio.connect(urlunparse(url))
+
+    def edit_tokens(self):
+        content = self.session.post(self.site + "api.php", params={
+            "action": "query",
+            "prop": "info",
+            "titles": "Main Page",
+            "intoken": "edit|delete|protect|move|block|unblock|email|import",
+            "format": "json",
+        }).json()
+        thes = tuple(content["query"]["pages"].values())[0]
+        try:
+            warnings = content["warnings"]["info"]["*"]
+        except:
+            warnings = None
+        if warnings is None:
+            warnings = {}
+        self.edit_token = thes["edittoken"] if "edit" not in warnings else None
+        self.delete_token = thes["deletetoken"] if "delete" not in warnings else None
+        self.protect_token = thes["protecttoken"] if "protect" not in warnings else None
+        self.move_token = thes["movetoken"] if "move" not in warnings else None
+        self.block_token = thes["blocktoken"] if "block" not in warnings else None
+        self.unblock_token = thes["unblocktoken"] if "unblock" not in warnings else None
+        self.email_token = thes["emailtoken"] if "email" not in warnings else None
+        self.import_token = thes["importtoken"] if "import" not in warnings else None
+
+    def view(self, title, section=None):
+        data = {
+            "action": "query",
+            "prop": "revisions",
+            "titles": title,
+            "rvprop": "timestamp|content",
+            "format": "json",
+        }
+        if section is not None:
+            data["rvsection"] = section
+        content = self.session.post(self.site + "api.php", data=data).json()
+        with open("edit.json", "w") as file:
+            json.dump(content, file)
+
+        thes = tuple(content["query"]["pages"].values())[0]
+        return thes["revisions"][0].get("*")
+
+    def edit(self, title, page_text, summary="", minor=False, bot=True, section=False):
+        data = {
+            "action": "edit",
+            "title": title,
+            "summary": summary,
+            "token": self.edit_token,
+            "format": "json",
+        }
+        try:
+            data["text"] = page_text.encode("utf-8")
+        except:
+            data["text"] = page_text
+        if bot:
+            data["bot"] = True
+        if minor:
+            data["minor"] = True
+        if section:
+            data["section"] = True
+        if page_text:
+            return self.session.post(self.site + "api.php", data=data).json()
 
     def send(self, attrs):
         self.sio.send(json.dumps({
@@ -75,30 +136,31 @@ class Client(ABC):
             "text": text,
         })
 
-    def kick_user(self, user):
+    def kick(self, username):
         self.send({
             "msgType": "command",
             "command": "kick",
-            "userToKick": user
+            "userToKick": username,
         })
 
     def logout(self):
         self.send({
             "msgType": "command",
-            "command": "logout"
+            "command": "logout",
         })
 
-    def connect(self):
+    def on_connect(self):
         print(f"Logged in as {self.username}")
 
-    def connect_error(self):
+    def on_connect_error(self):
         print("connect_error")
 
-    def disconnect(self):
+    def on_disconnect(self):
         print("disconnect")
 
     def on_event(self, data):
         handler = {
+            "initial": self.on_initial,
             "join": self.on_join,
             "logout": self.on_logout,
             "part": self.on_logout,
@@ -106,25 +168,24 @@ class Client(ABC):
             "ban": self.on_ban,
             "chat:add": self.on_message,
         }.get(data["event"])
+        print(data["event"])
         if handler is not None:
             handler(json.loads(data["data"]))
 
-    @abstractmethod
+    def on_initial(self, data):
+        pass
+
     def on_join(self, data):
         pass
 
-    @abstractmethod
     def on_logout(self, data):
         pass
 
-    @abstractmethod
     def on_kick(self, data):
         pass
 
-    @abstractmethod
     def on_ban(self, data):
         pass
 
-    @abstractmethod
     def on_message(self, data):
         pass
--- main.py
+++ main.py
@@ -1,24 +1,90 @@
 import sys
 import json
-import time
+import html
+from functools import wraps
 from threading import Timer
+from datetime import datetime
+from enum import Enum
 from client import Client, ClientError
+
+commands = {}
+
+class Rank(Enum):
+    REGULAR = 1
+    MODERATOR = 2
+    ADMIN = 3
+
+class User:
+    def __init__(self, name, rank):
+        self.name = name
+        self.rank = rank
 
 class ChatBot(Client):
     def __init__(self, config):
-        super().__init__(config["username"], config["password"], f'https://{config["wiki"]}.fandom.com/', config["youtube-key"])
+        super().__init__(config["username"], config["password"], f'https://{config["wiki"]}.fandom.com/')
+        self.users = {}
 
+    def command(name, min_rank=Rank.REGULAR):
+        def inner(handler):
+            @wraps(handler)
+            def wrapper(self, data):
+                username = data["attrs"]["name"]
+                user = self.users[username]
+                if user.rank >= min_rank:
+                    handler(self, data)
+            commands[name] = wrapper
+            return wrapper
+        return inner
+
+    @command("exit")
+    def exit(self, data):
+        sys.exit()
+
+    @command("kick", Rank.MODERATOR)
+    def kick(self, data):
+        message = data["attrs"]["text"]
+        username = message.split()[-1]
+        self.kick(username)
+
+    @command("update", Rank.MODERATOR)
+    def update_logs(self, data):
+        self.log_chat()
     def start(self):
         self.login()
-        log_timer = Timer(3600, self.chat_log)
-        log_timer.daemon = True
-        log_timer.start()
+        Timer(3600 - datetime.utcnow().minute * 60 - datetime.utcnow().second, self.hourly).start()
+
+    @command("hello")
+    def hello(self, data):
+        username = data["attrs"]["name"]
+        self.send_message(f'Hello there, {username}')
+
+    def hourly(self):
+        self.log_chat()
+        Timer(3600 - datetime.utcnow().minute * 60 - datetime.utcnow().second, self.hourly).start()
+
+    @staticmethod
+    def log(data):
+        timestamp = datetime.utcnow().strftime("[%Y-%m-%d %H:%M:%S]")
+        print(f"{timestamp} {data}")
+        with open("chat_log", "a") as log_file:
+            log_file.write(f"{timestamp} {html.escape(data)}\n")
+
+    def on_initial(self, data):
+        print(data)
 
     def on_join(self, data):
-        print(f'{time.strftime("[%Y-%m-%d %H:%M:%S]", time.gmtime())} -!- {data["attrs"]["name"]} has joined Special:Chat')
+        username = data["attrs"]["name"]
+        self.log(f'-!- {username} has joined Special:Chat')
+        if data["attrs"]["canPromoteModerator"]:
+            rank = Rank.ADMIN
+        elif data["attrs"]["isModerator"]:
+            rank = Rank.MODERATOR
+        else:
+            rank = Rank.REGULAR
+        self.users[username] = User(username, rank)
 
     def on_logout(self, data):
-        print(f'{time.strftime("[%Y-%m-%d %H:%M:%S]", time.gmtime())} -!- {data["attrs"]["name"]} has left Special:Chat')
+        self.log(f'-!- {data["attrs"]["name"]} has left Special:Chat')
 
     def on_kick(self, data):
         pass
@@ -27,12 +93,28 @@ class ChatBot(Client):
         pass
 
     def on_message(self, data):
-        print(f'Received a message from {data["attrs"]["name"]}: "{data["attrs"]["text"]}"')
-        if data["attrs"]["text"].startswith("!hello"):
-            self.send_message(f'Hello there, {data["attrs"]["name"]}')
+        username = data["attrs"]["name"]
+        message = data["attrs"]["text"]
+        self.log(f"<{username}> {message}")
+        if message.lstrip().startswith("!"):
+            command_name = message.split()[0][1:]
+            handler = commands.get(command_name)
+            if handler is not None:
+                handler(self, data)
 
-    def chat_log(self):
-        pass
+    def log_chat(self):
+        title = f'Project:Chat/Logs/{datetime.utcnow().strftime("%d %B %Y")}'
+        with open("chat_log") as log_file:
+            log_data = log_file.read()
+        with open("chat_log", "w") as log_file:
+            pass
+        page_text = self.view(title)
+        if page_text:
+            end = page_text.rindex("</pre>")
+            page_text = f"{page_text[:end]}\n{log_data}{page_text[end:]}"
+        else:
+            page_text = f'<pre>\n{log_data}\n</pre>\n[[Category:Chat logs/{datetime.utcnow().strftime("%Y %d %B")}]]'
+        self.edit(title, page_text, summary="Updating chat logs")
 
 def main():
     try:
