636fc5dc73558f26436497fc40a7bd6382e954ce
turicfr <turicfr@gmail.com>
2020-04-09 01:28:06 +0300

    Merge chatbot/client.py into chatbot/__init__.py

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,12 +1,30 @@
+import json
+from urllib.parse import urlencode, urlparse, urlunparse
 from datetime import datetime
 
-from .client import Client, ClientError
 from .users import User, Rank, RankError
 from .plugins import Plugin, Command, Argument, ArgumentError
 
-class ChatBot(Client):
+import requests
+import socketio
+
+class ClientError(Exception):
+    pass
+
+class ChatBot:
     def __init__(self, username, password, site):
-        super().__init__(username, password, site)
+        self.username = username
+        self.password = password
+        self.site = site
+        self.session = requests.Session()
+        self.sio = socketio.Client()
+        for event, handler in {
+            "connect": self.on_connect,
+            "connect_error": self.on_connect_error,
+            "disconnect": self.on_disconnect,
+            "message": self.on_event,
+        }.items():
+            self.sio.on(event, handler)
         self.users = {}
         self.plugins = []
 
@@ -16,28 +34,171 @@ class ChatBot(Client):
 
     def start(self):
         print(f"Logging in as {self.username}...")
-        self.login()
+        data = {
+            "action": "login",
+            "lgname": self.username,
+            "lgpassword": self.password,
+            "format": "json",
+        }
+        response = self.session.post(self.site + "api.php", data=data).json()
+        data["lgtoken"] = response["login"]["token"]
+        response = self.session.post(self.site + "api.php", data=data).json()
+        result = response["login"]["result"]
+        if result != "Success":
+            raise ClientError(f"Log in failed: {result}")
+        self.edit_tokens()
+        self.connect()
+
+    def connect(self):
+        wikia_data = self.session.get(self.site + "wikia.php", params={
+            "controller": "Chat",
+            "format": "json",
+        }).json()
+        api_data = self.session.get(self.site + "api.php", params={
+            "action": "query",
+            "meta": "siteinfo",
+            "siprop": "wikidesc",
+            "format": "json",
+        }).json()
+        url = list(urlparse(f'https://{wikia_data["chatServerHost"]}/socket.io/'))
+        url[4] = urlencode({
+            "name": self.username,
+            "key": wikia_data["chatkey"],
+            "roomId": wikia_data["roomId"],
+            "serverId": api_data["query"]["wikidesc"]["id"],
+        })
+        self.sio.connect(urlunparse(url))
+
+    def edit_tokens(self):
+        content = self.session.post(self.site + "api.php", params={
+            "action": "query",
+            "prop": "info",
+            "titles": "Main Page",
+            "intoken": "edit|delete|protect|move|block|unblock|email|import",
+            "format": "json",
+        }).json()
+        thes = tuple(content["query"]["pages"].values())[0]
+        try:
+            warnings = content["warnings"]["info"]["*"]
+        except:
+            warnings = None
+        if warnings is None:
+            warnings = {}
+        self.edit_token = thes["edittoken"] if "edit" not in warnings else None
+        self.delete_token = thes["deletetoken"] if "delete" not in warnings else None
+        self.protect_token = thes["protecttoken"] if "protect" not in warnings else None
+        self.move_token = thes["movetoken"] if "move" not in warnings else None
+        self.block_token = thes["blocktoken"] if "block" not in warnings else None
+        self.unblock_token = thes["unblocktoken"] if "unblock" not in warnings else None
+        self.email_token = thes["emailtoken"] if "email" not in warnings else None
+        self.import_token = thes["importtoken"] if "import" not in warnings else None
+
+    def view(self, title, section=None):
+        data = {
+            "action": "query",
+            "prop": "revisions",
+            "titles": title,
+            "rvprop": "timestamp|content",
+            "format": "json",
+        }
+        if section is not None:
+            data["rvsection"] = section
+        content = self.session.post(self.site + "api.php", data=data).json()
+        thes = tuple(content["query"]["pages"].values())[0]
+        try:
+            return thes["revisions"][0]["*"]
+        except KeyError:
+            return None
+
+    def edit(self, title, page_text, summary="", minor=False, bot=True, section=False):
+        data = {
+            "action": "edit",
+            "title": title,
+            "summary": summary,
+            "token": self.edit_token,
+            "format": "json",
+        }
+        try:
+            data["text"] = page_text.encode("utf-8")
+        except:
+            data["text"] = page_text
+        if bot:
+            data["bot"] = True
+        if minor:
+            data["minor"] = True
+        if section:
+            data["section"] = True
+        if page_text:
+            return self.session.post(self.site + "api.php", data=data).json()
+
+    def send(self, attrs):
+        self.sio.send(json.dumps({
+            "id": None,
+            "attrs": attrs,
+        }))
+
+    def send_message(self, text):
+        self.send({
+            "msgType": "chat",
+            "name": self.username,
+            "text": text,
+        })
+
+    def kick(self, username):
+        self.send({
+            "msgType": "command",
+            "command": "kick",
+            "userToKick": username,
+        })
+
+    def ban(self, username, duration, reason):
+        self.send({
+            "msgType": "command",
+            "command": "ban",
+            "userToBan": username,
+            "time": duration,
+            "reason": reason,
+        })
+
+    def logout(self):
+        self.send({
+            "msgType": "command",
+            "command": "logout",
+        })
 
     def on_connect(self):
-        super().on_connect()
         print(f"Logged in as {self.username}.")
         for plugin in self.plugins:
             plugin.on_connect()
 
     def on_connect_error(self):
-        super().on_connect_error()
         print("Connect error")
         for plugin in self.plugins:
             plugin.on_connect_error()
 
     def on_disconnect(self):
-        super().on_disconnect()
         print("Logged off")
         for plugin in self.plugins:
             plugin.on_disconnect()
 
+    def on_event(self, data):
+        handler = {
+            "initial": self.on_initial,
+            "join": self.on_join,
+            "logout": self.on_logout,
+            "part": self.on_logout,
+            "kick": self.on_kick,
+            "ban": self.on_ban,
+            "chat:add": self.on_message,
+        }.get(data["event"])
+        if handler is not None:
+            handler(json.loads(data["data"]))
+
     def on_join(self, data):
-        super().on_join(data)
+        self.send({
+            "msgType": "command",
+            "command": "initquery",
+        })
         username = data["attrs"]["name"]
         rank = Rank.from_attrs(data["attrs"])
         self.users[username.lower()] = User(username, rank, datetime.utcnow())
@@ -45,7 +206,6 @@ class ChatBot(Client):
             plugin.on_join(data)
 
     def on_initial(self, data):
-        super().on_initial(data)
         for user in data["collections"]["users"]["models"]:
             attrs = user["attrs"]
             username = attrs["name"]
@@ -55,7 +215,6 @@ class ChatBot(Client):
             plugin.on_initial(data)
 
     def on_logout(self, data):
-        super().on_logout(data)
         username = data["attrs"]["name"]
         user = self.users[username.lower()]
         user.connected = False
@@ -64,17 +223,14 @@ class ChatBot(Client):
             plugin.on_logout(data)
 
     def on_kick(self, data):
-        super().on_kick(data)
         for plugin in self.plugins:
             plugin.on_kick(data)
 
     def on_ban(self, data):
-        super().on_ban(data)
         for plugin in self.plugins:
             plugin.on_ban(data)
 
     def on_message(self, data):
-        super().on_message(data)
         for plugin in self.plugins:
             plugin.on_message(data)
         username = data["attrs"]["name"]
