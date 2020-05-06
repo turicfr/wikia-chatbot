a7bf38a35155c9d64a9b569da0da19d5af3517f3
turicfr <turicfr@gmail.com>
2020-05-06 13:30:19 +0300

    Prevent multiple tell messages

--- plugins/tell.py
+++ plugins/tell.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+from contextlib import contextmanager
 
 from chatbot.users import User
 from chatbot.plugins import Plugin, Command, Argument
@@ -8,38 +9,43 @@ from chatbot.plugins import Plugin, Command, Argument
 class TellPlugin:
     def __init__(self):
         self.client = None
-        self.joined = []
+        self.just_joined = set()
 
-    def on_load(self, client):
-        self.client = client
-
-    def on_join(self, data):
-        self.joined.append(data["attrs"]["name"])
-
-    def on_message(self, data):
-        username = data["attrs"]["name"]
-        if not username in self.joined:
-            return
-
+    @staticmethod
+    @contextmanager
+    def open_tell(write=True):
         try:
             with open("tell.json", encoding="utf-8") as tell_file:
                 tell = json.load(tell_file)
         except (FileNotFoundError, json.decoder.JSONDecodeError):
             tell = {}
+        try:
+            yield tell
+        finally:
+            if write:
+                with open("tell.json", "w", encoding="utf-8") as tell_file:
+                    json.dump(tell, tell_file)
 
-        messages = tell.get(username.lower())
-        if not messages:
+    def on_load(self, client):
+        self.client = client
+
+    def on_join(self, data):
+        self.just_joined.add(data["attrs"]["name"])
+
+    def on_message(self, data):
+        username = data["attrs"]["name"]
+        if username not in self.just_joined:
             return
-        for message in messages:
-            if not "delivered" in message:
-                self.client.send_message(
-                    f'{username}, {message["from"]} wanted to tell you @ '
-                    f'{datetime.utcfromtimestamp(message["timestamp"]):%Y-%m-%d %H:%M:%S} UTC: {message["message"]}'
-                )
-                message["delivered"] = datetime.utcnow().timestamp()
-        self.joined.remove(username)
-        with open("tell.json", "w", encoding="utf-8") as tell_file:
-            json.dump(tell, tell_file)
+        self.just_joined.remove(username)
+
+        with self.open_tell() as tell:
+            for message in tell.get(username.lower(), []):
+                if "delivered" not in message:
+                    self.client.send_message(
+                        f'{username}, {message["from"]} wanted to tell you @ '
+                        f'{datetime.utcfromtimestamp(message["timestamp"]):%Y-%m-%d %H:%M:%S} UTC: {message["message"]}'
+                    )
+                    message["delivered"] = datetime.utcnow().timestamp()
 
     @Command(
         sender=Argument(implicit=True),
@@ -48,7 +54,7 @@ class TellPlugin:
         message=Argument(rest=True),
     )
     def tell(self, sender, timestamp, target, message):
-        """Deliver an offline user a message when he joins the chat."""
+        """Deliver an offline user a message."""
         if sender == target:
             self.client.send_message(f"{sender}, you can't leave a message to yourself.")
             return
@@ -57,74 +63,56 @@ class TellPlugin:
             self.client.send_message(f"{target} is already here.")
             return
 
-        try:
-            with open("tell.json", encoding="utf-8") as tell_file:
-                tell = json.load(tell_file)
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            tell = {}
-
-        if target.name.lower() not in tell:
-            tell[target.name.lower()] = []
-
-        tell[target.name.lower()].append({
-            "from": sender.name,
-            "message": message,
-            "timestamp": timestamp.timestamp(),
-        })
-        with open("tell.json", "w", encoding="utf-8") as tell_file:
-            json.dump(tell, tell_file)
+        with self.open_tell() as tell:
+            messages = list(filter(lambda m: m["from"] != sender.name, tell.get(target.name.lower(), [])))
+            messages.append({
+                "from": sender.name,
+                "message": message,
+                "timestamp": timestamp.timestamp(),
+            })
+            tell[target.name.lower()] = messages
         self.client.send_message(f"I'll tell {target} that the next time I see them.")
 
-    @Command(
-        sender=Argument(implicit=True),
-        target=Argument(type=User, required=False)
-    )
+    @Command(sender=Argument(implicit=True), target=Argument(type=User, required=False))
     def told(self, sender, target=None):
         """Report the status of your pending tell messages."""
-        try:
-            with open("tell.json", encoding="utf-8") as tell_file:
-                tell = json.load(tell_file)
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            tell = {}
-
-        response = []
         if target is None:
-            for user, messages in tell.items():
-                count = sum(1 for message in messages if message["from"] == sender.name)
-                if not count:
-                    continue
-                response.append(
-                    f'there {"is" if count == 1 else "are"} {count} '
-                    f'message{"" if count == 1 else "s"} pending from you to {user}.'
-                )
+            response = []
+            with self.open_tell(write=False) as tell:
+                for user, messages in tell.items():
+                    if messages:
+                        response.append(f"there is a message pending from you to {user}.")
             if not response:
                 response = ["you currently don't have tell messages to anyone."]
+            self.client.send_message("\n".join(f"{sender}, {line}" for line in response))
         else:
-            if target.name.lower() == sender.name.lower():
-                response = ["you can't tell yourself."]
-            else:
-                for message in filter(lambda m: m["from"] == sender.name, tell.get(target.name.lower(), [])):
-                    message_text = " ".join(message["message"].split()[:5])
-                    if len(message["message"].split()) > 5:
-                        message_text += "..."
-                    delivered = message.get("delivered")
-                    if delivered is not None:
-                        response.append(
-                            f'I delivered your message "{message_text}" to {target} '
-                            f"on {datetime.utcfromtimestamp(delivered):%Y-%m-%d %H:%M:%S} UTC."
-                        )
-                    else:
-                        response.append(f'I haven\'t been able to deliver your message "{message_text}" to {target} yet.')
-                if not response:
-                    response = [f"I've got no message from you to {target}."]
+            if sender == target:
+                self.client.send_message(f"{sender}, you can't tell yourself.")
+                return
 
-                if tell.get(target.name.lower()):
-                    tell[target.name.lower()] = list(filter(
-                        lambda m: m["from"] != sender.name or "delivered" not in m,
-                        tell[target.name.lower()]
-                    ))
-                    if not tell[target.name.lower()]:
-                        del tell[target.name.lower()]
-                    with open("tell.json", "w", encoding="utf-8") as tell_file:
-                        json.dump(tell, tell_file)
-        self.client.send_message("\n".join(f"{sender}, {line}" for line in response))
+            with self.open_tell() as tell:
+                messages = tell.get(target.name.lower(), [])
+                try:
+                    message = next(m for m in messages if m["from"] == sender.name)
+                except StopIteration:
+                    self.client.send_message(f"{sender}, I've got no message from you to {target}.")
+                    return
+
+                text = " ".join(message["message"].split()[:5])
+                if len(message["message"].split()) > 5:
+                    text += "..."
+                delivered = message.get("delivered")
+                if delivered is None:
+                    self.client.send_message(
+                        f"{sender}, I haven't been able to deliver your "
+                        f'message "{text}" to {target} yet.'
+                    )
+                    return
+
+                messages.remove(message)
+                if not messages:
+                    del tell[target.name.lower()]
+            self.client.send_message(
+                f'{sender}, I delivered your message "{text}" to {target} '
+                f"on {datetime.utcfromtimestamp(delivered):%Y-%m-%d %H:%M:%S} UTC."
+            )
