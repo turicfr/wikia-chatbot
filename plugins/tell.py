571c95b4916f8ee2b20b137639f163486c11fbba
turicfr <turicfr@gmail.com>
2020-05-12 14:31:52 +0300

    Filter delivered messages

--- plugins/tell.py
+++ plugins/tell.py
@@ -84,7 +84,7 @@ class TellPlugin:
             response = []
             with self.open_tell(write=False) as tell:
                 for user, messages in tell.items():
-                    if list(filter(lambda m: m["from"] == sender.name, messages)):
+                    if list(filter(lambda m: m["from"] == sender.name and "delivered" not in m, messages)):
                         response.append(f"there is a message pending from you to {user}.")
             if not response:
                 response = ["you currently don't have tell messages to anyone."]
